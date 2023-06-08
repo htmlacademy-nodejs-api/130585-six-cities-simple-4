9fe7a29dd797a949545f07cf09a00f7e76b4c5e7
Ekaterina Kireeva <ekireeva@fun.co>
2023-06-08 22:19:09 +0300

    [Сервер наносит ответный удар]: возврат только нужных данных для городов

--- src/modules/city/city.controller.ts
+++ src/modules/city/city.controller.ts
@@ -4,8 +4,10 @@ import { Request, Response } from 'express';
 import { Controller } from '@core/controller/controller.abstract.js';
 import { LoggerInterface } from '@core/logger/logger.interface.js';
 import { CityServiceInterface } from '@modules/city/city-service.interface.js';
+import CityRdo from '@modules/city/rdo/city.rdo.js';
 import { AppComponent } from '@appTypes/app-component.enum.js';
 import { HttpMethod } from '@appTypes/http-method.enum.js';
+import { fillDTO } from '@utils/db.js';
 
 @injectable()
 export default class CityController extends Controller {
@@ -17,13 +19,23 @@ export default class CityController extends Controller {
 
     this.logger.info('Регистрация маршрутов для CategoryController…');
 
-    this.addRoute({path: '/', method: HttpMethod.Get, handler: this.index});
-    this.addRoute({path: '/', method: HttpMethod.Post, handler: this.create});
+    this.addRoute({
+      path: '/',
+      method: HttpMethod.Get,
+      handler: this.index,
+    });
+    this.addRoute({
+      path: '/',
+      method: HttpMethod.Post,
+      handler: this.create,
+    });
   }
 
   public async index(_req: Request, res: Response): Promise<void> {
-    const categories = await this.cityService.find();
-    this.ok(res, categories);
+    const cities = await this.cityService.find();
+    const citiesToResponse = fillDTO(CityRdo, cities);
+
+    this.ok(res, citiesToResponse);
   }
 
   public create(_req: Request, _res: Response): void {
