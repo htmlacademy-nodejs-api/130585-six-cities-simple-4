cff47ac574d7c3822d5f20f80c268041c74fabd0
Ekaterina Kireeva <ekireeva@fun.co>
2023-06-26 09:15:20 +0300

    [Подготовка к защите]: фикс сохранения комментария

--- src/modules/comment/dto/create-comment.dto.ts
+++ src/modules/comment/dto/create-comment.dto.ts
@@ -1,17 +1,21 @@
 import { Length,IsMongoId, IsInt, Max, Min } from 'class-validator';
+import { Expose } from 'class-transformer';
 
 import { CommentTextValidation, RentRatingValidation } from '@const/validation.js';
 import { CommentTextError, CommentRentIdError, RentRatingError } from '@const/error-messages.js';
 
 export default class CreateCommentDto {
+  @Expose()
   @Length(CommentTextValidation.Min, CommentTextValidation.Max, { message: CommentTextError.Length })
   public text!: string;
 
+  @Expose()
   @IsMongoId({ message: CommentRentIdError.IsMongoId })
   public rentId!: string;
 
   public author!: string;
 
+  @Expose()
   @IsInt({ message: RentRatingError.Int })
   @Min(RentRatingValidation.Min, { message: RentRatingError.Min })
   @Max(RentRatingValidation.Max, { message: RentRatingError.Max })
